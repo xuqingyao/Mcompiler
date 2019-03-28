964f553ed442de1d8e9d1c85f58cdf99ce4a4d2c
echo <33317148+xuqingyao@users.noreply.github.com>
2019-03-28 13:59:52 +0800

    Update Main.java

--- src/com/echo/compiler/frontend/Main.java
+++ src/com/echo/compiler/frontend/Main.java
@@ -33,8 +33,8 @@ public class Main {
     }
 
     private static void buildAST() throws Exception {
-//        String inFile = null;
-        String inFile = "C:/Users/echo/Desktop/M_compiler/src/com/echo/compiler/testcase/testcase_174.txt";
+       String inFile = null;
+//         String inFile = "C:/Users/echo/Desktop/M_compiler/src/com/echo/compiler/testcase/testcase_174.txt";
 //        String inFile = "C:/Users/echo/Desktop/1.txt";
 
         InputStream in;
