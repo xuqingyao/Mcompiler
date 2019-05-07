0d9a949dba0efdd4f1cbaf41b03c2ecd12eafed6
xuqingyao <xuqingyao@sjtu.edu.cn>
2019-05-07 18:53:32 +0800

    add optim bash

---
+++ optim.bash
@@ -0,0 +1,8 @@
+# this script is called when the judge wants our compiler to compile a source file.
+# print the compiled source, i.e. asm code, directly to stdout.
+# don't print anything other to stdout.
+# if you would like to print some debug information, please go to stderr.
+
+set -e
+cd "$(dirname "$0")"
+java -classpath ./lib/antlr-4.7.2-complete.jar:./bin com.echo.compiler.frontend.Main 
